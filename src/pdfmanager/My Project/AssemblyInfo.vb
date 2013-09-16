d35de26912a247f2fc3a6c8306dab1320eec8237
udayan <udaroy@rediffmail.com>
2013-09-16 10:10:23 +0530

    move to WPF & .NET 4

--- src/pdfmanager/My Project/AssemblyInfo.vb
+++ src/pdfmanager/My Project/AssemblyInfo.vb
@@ -1,8 +1,9 @@
-﻿Imports System.Resources
-
-Imports System
+﻿Imports System
 Imports System.Reflection
 Imports System.Runtime.InteropServices
+Imports System.Globalization
+Imports System.Resources
+Imports System.Windows
 
 ' General Information about an assembly is controlled through the following 
 ' set of attributes. Change these attribute values to modify the information
@@ -11,16 +12,37 @@ Imports System.Runtime.InteropServices
 ' Review the values of the assembly attributes
 
 <Assembly: AssemblyTitle("pdfmanager")> 
-<Assembly: AssemblyDescription("PDFManager convert Images to PDF file. And its supports popular image type like JPEG, TIFF, PNG, EMF WMF")> 
-<Assembly: AssemblyCompany("Udayan Roy")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("")> 
 <Assembly: AssemblyProduct("pdfmanager")> 
 <Assembly: AssemblyCopyright("Copyright ©  2013")> 
 <Assembly: AssemblyTrademark("")> 
+<Assembly: ComVisible(false)>
 
-<Assembly: ComVisible(False)> 
+'In order to begin building localizable applications, set 
+'<UICulture>CultureYouAreCodingWith</UICulture> in your .vbproj file
+'inside a <PropertyGroup>.  For example, if you are using US english 
+'in your source files, set the <UICulture> to "en-US".  Then uncomment the
+'NeutralResourceLanguage attribute below.  Update the "en-US" in the line
+'below to match the UICulture setting in the project file.
+
+'<Assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)> 
+
+
+'The ThemeInfo attribute describes where any theme specific and generic resource dictionaries can be found.
+'1st parameter: where theme specific resource dictionaries are located
+'(used if a resource is not found in the page, 
+' or application resource dictionaries)
+
+'2nd parameter: where the generic resource dictionary is located
+'(used if a resource is not found in the page, 
+'app, and any theme specific resource dictionaries)
+<Assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)>
+
+
 
 'The following GUID is for the ID of the typelib if this project is exposed to COM
-<Assembly: Guid("da46093b-2bb9-42e9-af07-c047367082e8")> 
+<Assembly: Guid("d146cb8c-629e-4617-90be-d32401807075")> 
 
 ' Version information for an assembly consists of the following four values:
 '
@@ -33,7 +55,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("0.1")> 
-<Assembly: AssemblyFileVersion("0.1")> 
-
-<Assembly: NeutralResourcesLanguageAttribute("en")> 
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
